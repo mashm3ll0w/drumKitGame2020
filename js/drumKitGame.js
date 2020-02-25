a60a73e3f4ecbfffe19d256364a44558b7e0b891
Charles Swaleh <charlswall@live.com>
2020-02-25 20:56:13 +0300

    add the animation to the pressed key element

--- js/drumKitGame.js
+++ js/drumKitGame.js
@@ -6,4 +6,10 @@ document.addEventListener("keydown", function(e){ // add eventlistener
   
   audio.currentTime  = 0; // This ensures that the audio always starts from the beginning when the key is pressed repeatedly
   audio.play(); // play  the audio
+
+  const pressedKey = document.querySelector(`div[data-key="${e.keyCode}"]`); // select the square whose sound you need to play
+  pressedKey.classList.add("playing");  // add the class of "playing" to the pressed square to scale it up some
+  setTimeout(() => {
+    pressedKey.classList.remove("playing"); // timeout to remove the class of playing after 0.05 seconds
+  }, 50);
 });
